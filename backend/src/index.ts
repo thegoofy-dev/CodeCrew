b8a0a77da93ff445518c28b5c5e03b151aba7d31
Pankaj Tyagi <itstyagi0001@gmail.com>
2025-05-01 12:19:15 +0530

    Initialized Express server with env-based config

---
+++ backend/src/index.ts
@@ -0,0 +1,40 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        name: "session",
+        keys: [config.SESSION_SECRET],
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: config.NODE_ENV === "production",
+        httpOnly: true,
+        sameSite: "lax"
+    })
+)
+
+app.use(
+    cors({
+        origin: config.FRONTEND_ORIGIN,
+        credentials: true,
+    })
+)
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        message: "Backend part of CodeCrew"
+    })
+})
+
+app.listen(config.PORT, async () => {
+    console.log(`Server is running on port ${config.PORT} in ${config.NODE_ENV}`)
+})
