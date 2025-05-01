ae5c18e98d3b2e491c3354dcf628f4a41839b913
Pankaj Tyagi <itstyagi0001@gmail.com>
2025-05-01 14:50:27 +0530

    Implement error handling middleware and custom error classes; add HTTP status codes and error codes enumeration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,11 @@ import cors from "cors";
 import session from "cookie-session";
 import { config } from "./config/app.config";
 import connectDatabase from "./config/db.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { BadRequestException } from "./utils/appError";
+import { ErrorCodeEnum } from "./enums/error-code.enum";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -19,24 +24,27 @@ app.use(
         maxAge: 24 * 60 * 60 * 1000,
         secure: config.NODE_ENV === "production",
         httpOnly: true,
-        sameSite: "lax"
+        sameSite: "lax",
     })
-)
+);
 
 app.use(
     cors({
         origin: config.FRONTEND_ORIGIN,
         credentials: true,
     })
-)
+);
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({
-        message: "Backend part of CodeCrew"
-    })
-})
+app.get(`/`, asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    throw new BadRequestException("This is a bad request", ErrorCodeEnum.AUTH_INVALID_TOKEN);
+    res.status(HTTPSTATUS.OK).json({
+        message: "Backend part of CodeCrew",
+    });
+}));
+
+app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
-    console.log(`Server is running on port ${config.PORT} in ${config.NODE_ENV}`)
+    console.log(`Server is running on port ${config.PORT} in ${config.NODE_ENV}`);
     await connectDatabase();
-})
+});
