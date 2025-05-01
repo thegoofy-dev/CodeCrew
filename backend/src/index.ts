f1ee709cf9fabbb3075189f5d117db750d1dcb0f
Pankaj Tyagi <itstyagi0001@gmail.com>
2025-05-01 12:42:30 +0530

    Added Mongo database connection utility and integrate it into the server startup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import session from "cookie-session";
 import { config } from "./config/app.config";
+import connectDatabase from "./config/db.config";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -37,4 +38,5 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
 
 app.listen(config.PORT, async () => {
     console.log(`Server is running on port ${config.PORT} in ${config.NODE_ENV}`)
+    await connectDatabase();
 })
